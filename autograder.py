ab984098da3902e1f540e38c2e50695eda29b5b4
alanmmckay <alanmmckay@gmail.com>
2022-05-13 22:38:43 -0500

    Initial progress. Need to reconsider parameters for grade_submissions(); is it necessary to have some of these when they can be included in the relevant tests file? Also need to consider whether or not insert_try_block() should be local to grade_submissions(). Next step is to also alllow execution of created test files within python instead of relying on shell scripting.

---
+++ autograder.py
@@ -0,0 +1,119 @@
+import os
+
+#os.getcwd()
+#os.listdir() returns the contents of cwd if no argument is given
+
+#A function which filters a set of characters from a given string
+def filter_characters_from_string(string,exemptChars):
+    newString = ''
+    for char in string:
+        if char not in exemptChars:
+            newString = newString + char
+    return newString
+
+
+#A function that filters characters from a set of strings
+def filter_characters_from_strings(strings,exemptChars,exemptStrings = [],flag = False):
+    newStrings = []
+    for string in strings:
+        if string not in exemptStrings:
+            newString = filter_characters_from_string(string,exemptChars)
+            if newString:
+                newStrings.append(newString)
+            elif flag == True:
+                newStrings.append(False)
+            #else throw an error...?
+    return newStrings
+
+
+def rename_file(fileName, newFileName, path = os.getcwd()):
+    try:
+        os.replace(path+fileName,path+newFileName)
+        return True
+    except:
+        return False
+
+
+def rename_files(files, exemptChars, exemptFiles = [], path = os.getcwd()):
+    # !!! Consider order of parameters!
+
+    newFileNames = filter_characters_from_strings(files,exemptChars,exemptFiles,True)
+    #assuming len(newFileNames) == len(files)
+    index = 0
+    length = len(newFileNames)
+    while index < length:
+        if newFileNames[index]:
+            success = rename_file(files[index],newFileNames[index],path)
+            #error handling for success
+            #probably a write to a log
+        index = index + 1
+    return newFileNames
+
+
+def insert_try_block(name,source):
+    tabSpace = '    '
+    string = 'try:\n'
+    string = string + tabSpace + 'from '+source+' import '+name+'\n'
+    string = string + 'except:\n'
+    string = string + tabSpace + 'print("Error in loading '+name+'")'+'\n'
+    return string
+
+
+def grade_submissions(testFunctions, path = os.getcwd(), shellScripts = 'files.sh', outputLog = 'output.txt', functionFile = '' , functions = [], libraryImports = [], testsFile = 'tests.py', exemptChars = [], exemptFiles = []):
+    # !!! Make file renaming an option! Implicit by default empty lists for exemptions
+
+    files = os.listdir(path)
+
+    # --- Rename files in directory --- #
+    if exemptChars:
+        # !!! Make sure path is properly propagated!
+        newFiles = rename_files(files, exemptChars, exemptFiles, path)
+        files = newFiles
+    # --- Otherwise make sure factor exemptFiles --- #
+    elif exemptFiles:
+        for file in exemptFiles:
+            files.remove(file)
+
+    # --- Sort impending file read for the sake of output --- #
+    files.sort()
+
+    # --- Prime test files --- #
+    f = open(testsFile,'r')
+    testData = f.read()
+    f.close()
+
+    tabSpace = '    '
+    shellScriptString = ''
+    for file in files:
+        nameVar = file.split('.')[0]
+        if file != shellScripts and file != outputLog and file != functionFile and file != testsFile:
+            newData = 'name = "'+nameVar+'"\n'
+            for function in libraryImports:
+                newData = newData + function + '\n'
+            for function in testFunctions:
+                newData = newData + insert_try_block(function,nameVar)
+            if functionFile:
+                for function in functions:
+                    newData = newData + insert_try_block(function,functionFile.split('.')[0])
+            newData = newData + 'print("'+file+'")\n'
+            #newData = newData + 'try:\n'
+            newData = newData + testData
+            newData = newData + '\nprint("---------------")\n'
+            newFile = open('test.'+file,'w')
+            newFile.write(newData)
+            newFile.close()
+
+            shellScriptString = shellScriptString + 'python3 test.'+file+' >> '+outputLog+'\n'
+            shellScriptString = shellScriptString + 'rm test.'+file+'\n'
+            print(file)
+
+    f = open(shellScripts,'a')
+    f.write(shellScriptString)
+    f.close()
+
+    return True
+#grade_submissions(testFunctions, path = os.getcwd(), shellScripts = 'files.sh', outputLog = 'output.txt', functionFile = '' , functions = [], libraryImports = [], testsFile = 'tests.py', exemptChars = [], exemptFiles = []):
+
+grade_submissions(['manageWindows','newWeek'],os.getcwd(),'files.sh','output.txt','func.py', ['parseRecord'],['from datetime import datetime, timedelta'],'tests.py',['-',' ','(',')'],['tests.py','grade.py','files.sh','func.py','output.txt','__pycache__'])
+
+
