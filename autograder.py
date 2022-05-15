9704def7deedd935a4cfe22667b80797df42f06a
alanmmckay <alanmmckay@gmail.com>
2022-05-14 21:54:21 -0500

    Started providing a means to handle dynamically generated test files and feedback output contained in seperate folders. Need to look into sys module for imports from different directories!

--- autograder.py
+++ autograder.py
@@ -2,6 +2,7 @@ import os
 
 #os.getcwd()
 #os.listdir() returns the contents of cwd if no argument is given
+#os.mkdir()
 
 #A function which filters a set of characters from a given string
 def filter_characters_from_string(string,exemptChars):
@@ -49,6 +50,14 @@ def rename_files(files, exemptChars, exemptFiles = [], path = os.getcwd()):
         index = index + 1
     return newFileNames
 
+def copy_file(fileName,origin,destination):
+    fileContents = open(origin+fileName,'r')
+    fileData = fileContents.read()
+    fileContents.close()
+    newFileContents = open(destination+fileName,'w')
+    newFileContents.write(fileData)
+    newFileContents.close()
+    return True
 
 def insert_try_import(name,source):
     tabSpace = '    '
@@ -84,25 +93,37 @@ def grade_submissions(testFunctions, path = os.getcwd(), testsFile = 'tests.py',
 
     tabSpace = '    '
     shellScriptString = ''
+
+    try:
+        os.mkdir('feedback')
+    except:
+        print('feedback folder already created!')
+
+    '''try:
+        os.mkdir('tests')
+    except:
+        print('tests folder already created!')'''
+
     for file in files:
         nameVar = file.split('.')[0]
-        if file != shellScripts and file != outputLog and file != functionFile and file != testsFile:
+        if file != shellScripts and file != outputLog and file != testsFile:
             newData = 'name = "'+nameVar+'"\n'
             for function in testFunctions:
                 newData = newData + insert_try_import(function,nameVar)
-            newData = newData + 'print("'+file+'")\n'
-            #newData = newData + 'try:\n'
+            newData = newData + '\nprint("'+file+'")\n'
             newData = newData + testData
             newData = newData + '\nprint("---------------")\n'
             newFile = open('test.'+file,'w')
             newFile.write(newData)
             newFile.close()
 
-            shellScriptString = shellScriptString + 'python3 test.'+file+' >> '+outputLog+'\n'
+            shellScriptString = shellScriptString + 'python3 test.'+file+' > feedback/'+nameVar+outputLog+'\n'
+            shellScriptString = shellScriptString + 'cat feedback/'+nameVar+outputLog+' >> '+outputLog+'\n'
             shellScriptString = shellScriptString + 'rm test.'+file+'\n'
             print(file)
 
-    f = open(shellScripts,'a')
+
+    f = open(shellScripts,'w')
     f.write(shellScriptString)
     f.close()
 
@@ -110,6 +131,6 @@ def grade_submissions(testFunctions, path = os.getcwd(), testsFile = 'tests.py',
 
 #def grade_submissions(testFunctions, path = os.getcwd(), testsFile = 'tests.py', outputLog = 'output.txt', shellScripts = 'files.sh', exemptChars = [], exemptFiles = []):
 
-grade_submissions(['manageWindows','newWeek'],os.getcwd(),'tests.py','files.sh','output.txt',['-',' ','(',')'],['tests.py','grade.py','files.sh','func.py','output.txt','__pycache__'])
+grade_submissions(['manageWindows','newWeek'],os.getcwd(),'tests.py','output.txt','files.sh',['-',' ','(',')'],['tests.py','autograder.py','files.sh','output.txt','__pycache__','.git','tests','feedback'])
 
 
